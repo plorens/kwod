d8576b6a88d9d62185f51e9a1d2deb70eb703e2e
plorens <lorenspiotr97@gmail.com>
2021-06-09 01:28:21 +0200

    Dodanie nowych obrazow/ danych. Poprawki kodu

--- mammoviewer.m
+++ mammoviewer.m
@@ -1,43 +1,45 @@
 clc;
 clear all;
 close all;
+sciezka='dane/P_02079_RIGHT_MLO.jpg';
 
-I = imread('dane/mammo.jpg');
-% I = imread('mammo.jpg');
+I = imread(sciezka);
 wymiary=size(I);
-I_przyciete = I(1:wymiary(1),1:wymiary(2));
+obraz_przyciety = I(1:wymiary(1),1:wymiary(2));
 
 % Obraz oryginalny
 figure()
-imshow(I_przyciete);
+imshow(obraz_przyciety);
 
+I_equalized = adapthisteq(obraz_przyciety);
 % Contrast-limited Adaptive Histogram Equalization
-I_equalized = adapthisteq(I_przyciete);
 figure()
 imshow(I_equalized);
 
-% Potrzebne do obrysu (tylko czarny i bialy)
-bw = im2bw(I_equalized, graythresh(I_equalized));
+bw1 = im2bw(I_equalized, graythresh(I_equalized));
+% Informacja gdzie jest obiekt na zdjeciu
 figure()
-imshow(bw);
+imshow(bw1);
 
-bw2 = imfill(bw,'holes');
+bw2 = imfill(bw1,'holes');
 bw3 = imopen(bw2,ones(5,5));
-bw4 = bwareaopen(bw3, 40);
-bw4_perim = bwperim(bw4);
-overlay1 = imoverlay(I_equalized, bw4_perim, 'red');
-% Obrys
+bw4 = bwareaopen(bw3, 30);
+bw5 = bwperim(bw4);
+obrys = imoverlay(I_equalized, bw5, 'red');
+% Obrys biektu
 figure()
-imshow(overlay1);
+imshow(obrys);
 
-mask_em = imextendedmax(I_equalized, 30);
+mask_em = imextendedmax(I_equalized, 40);
+% Wyznaczenie podejrzanych punktów
 figure()
 imshow(mask_em)
 
 mask_em = imclose(mask_em, ones(5,5));
 mask_em = imfill(mask_em, 'holes');
-mask_em = bwareaopen(mask_em, 40);
-overlay2 = imoverlay(I_equalized, bw4_perim | mask_em, 'red');
+mask_em = bwareaopen(mask_em, 100);
+result = imoverlay(I_equalized, bw5 | mask_em, 'red');
+% Obraz finalny
 figure()
-imshow(overlay2);
+imshow(result);
 
